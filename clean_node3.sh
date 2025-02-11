f5682fca05a8bf0afc4ff8937d0b51a767f8ca18
Carlos Henrique Freitas de Souza <henriquesouza@brha0003lt.local>
2025-02-11 13:38:45 -0300

    add log to helm execution process

--- clean_node3.sh
+++ clean_node3.sh
@@ -1,5 +1,18 @@
+# Removendo Node3 com Helm
+echo "🗑 Removendo Node3 MongoDB..."
 helm uninstall node3-mongo --namespace brldigital-t
+
+echo "🗑 Removendo Node3 PostgreSQL..."
 helm uninstall node3-postgre --namespace brldigital-t
+
+echo "🗑 Removendo Node3 Node..."
 helm uninstall node3-node --namespace brldigital-t
+
+echo "🗑 Removendo Node3 Executor..."
 helm uninstall node3-executor --namespace brldigital-t
+
+echo "🗑 Removendo Node3 Prover..."
 helm uninstall node3-prover --namespace brldigital-t
+
+echo "✅ Todas as remoções foram concluídas!"
+
