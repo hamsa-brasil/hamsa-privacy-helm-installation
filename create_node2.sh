814ddbc771855168f638dd42d5e0ff0ba4c6b04c
Carlos Henrique Freitas de Souza <henriquesouza@brha0003lt.local>
2025-02-14 16:18:52 -0300

    remove watch and debug

--- create_node2.sh
+++ create_node2.sh
@@ -1,21 +1,21 @@
 echo "🔹 Instalando Node2 MongoDB..."
-helm install node2-mongo --namespace brldigital-t -f mongodb/values-node2-mongodb.yaml mongodb/ --debug --wait
+helm install node2-mongo --namespace brldigital-t -f mongodb/values-node2-mongodb.yaml mongodb/ 
 echo "✅ Node2 MongoDB instalado com sucesso!"
 
 echo "🔹 Instalando Node2 PostgreSQL..."
-helm install node2-postgre --namespace brldigital-t -f postgres/values-node2-postgresql.yaml postgres/ --debug --wait
+helm install node2-postgre --namespace brldigital-t -f postgres/values-node2-postgresql.yaml postgres/ 
 echo "✅ Node2 PostgreSQL instalado com sucesso!"
 
 echo "🔹 Instalando Node2 Executor..."
-helm install node2-executor --namespace brldigital-t -f executor/values-node2-executor.yaml executor/ --debug --wait
+helm install node2-executor --namespace brldigital-t -f executor/values-node2-executor.yaml executor/ 
 echo "✅ Node2 Executor instalado com sucesso!"
 
 echo "🔹 Instalando Node2 Prover..."
-helm install node2-prover --namespace brldigital-t -f prover/values-node2-prover.yaml prover/ --debug --wait
+helm install node2-prover --namespace brldigital-t -f prover/values-node2-prover.yaml prover/ 
 echo "✅ Node2 Prover instalado com sucesso!"
 
 echo "🔹 Instalando Node2 Node..."
-helm install node2-node --namespace brldigital-t -f node/values-node2-node.yaml node/ --debug --wait
+helm install node2-node --namespace brldigital-t -f node/values-node2-node.yaml node/ 
 echo "✅ Node2 Node instalado com sucesso!"
 
 echo "🚀 Todas as instalações foram concluídas!"
--- create_node4.sh
+++ create_node4.sh
@@ -1,21 +1,21 @@
 echo "🔹 Instalando Node4 MongoDB..."
-helm install node4-mongo --namespace brldigital-t -f mongodb/values-node4-mongodb.yaml mongodb/ --debug --wait
+helm install node4-mongo --namespace brldigital-t -f mongodb/values-node4-mongodb.yaml mongodb/ 
 echo "✅ Node4 MongoDB instalado com sucesso!"
 
 echo "🔹 Instalando Node4 PostgreSQL..."
-helm install node4-postgre --namespace brldigital-t -f postgres/values-node4-postgresql.yaml postgres/ --debug --wait
+helm install node4-postgre --namespace brldigital-t -f postgres/values-node4-postgresql.yaml postgres/ 
 echo "✅ Node4 PostgreSQL instalado com sucesso!"
 
 echo "🔹 Instalando Node4 Executor..."
-helm install node4-executor --namespace brldigital-t -f executor/values-node4-executor.yaml executor/ --debug --wait
+helm install node4-executor --namespace brldigital-t -f executor/values-node4-executor.yaml executor/ 
 echo "✅ Node4 Executor instalado com sucesso!"
 
 echo "🔹 Instalando Node4 Prover..."
-helm install node4-prover --namespace brldigital-t -f prover/values-node4-prover.yaml prover/ --debug --wait
+helm install node4-prover --namespace brldigital-t -f prover/values-node4-prover.yaml prover/ 
 echo "✅ Node4 Prover instalado com sucesso!"
 
 echo "🔹 Instalando Node4 Node..."
-helm install node4-node --namespace brldigital-t -f node/values-node4-node.yaml node/ --debug --wait
+helm install node4-node --namespace brldigital-t -f node/values-node4-node.yaml node/ 
 echo "✅ Node4 Node instalado com sucesso!"
 
 echo "🚀 Todas as instalações foram concluídas!"
