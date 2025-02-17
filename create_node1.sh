dc9104cb2422ec2f05183d776bb6da45e54219c4
Carlos Henrique Freitas de Souza <henriquesouza@brha0003lt.local>
2025-02-17 16:43:29 -0300

    change install to helm upgrade install

--- create_node1.sh
+++ create_node1.sh
@@ -1,21 +1,21 @@
 echo "🔹 Instalando Node1 MongoDB..."
-helm install node1-mongo --namespace brldigital-t -f mongodb/values-node1-mongodb.yaml mongodb/
+helm upgrade --install node1-mongo --namespace brldigital-t -f mongodb/values-node1-mongodb.yaml mongodb/
 echo "✅ Node1 MongoDB instalado com sucesso!"
 
 echo "🔹 Instalando Node1 PostgreSQL..."
-helm install node1-postgre --namespace brldigital-t -f postgres/values-node1-postgresql.yaml postgres/
+helm upgrade --install node1-postgre --namespace brldigital-t -f postgres/values-node1-postgresql.yaml postgres/
 echo "✅ Node1 PostgreSQL instalado com sucesso!"
 
 echo "🔹 Instalando Node1 Executor..."
-helm install node1-executor --namespace brldigital-t -f executor/values-node1-executor.yaml executor/
+helm upgrade --install node1-executor --namespace brldigital-t -f executor/values-node1-executor.yaml executor/
 echo "✅ Node1 Executor instalado com sucesso!"
 
 echo "🔹 Instalando Node1 Prover..."
-helm install node1-prover --namespace brldigital-t -f prover/values-node1-prover.yaml prover/
+helm upgrade --install node1-prover --namespace brldigital-t -f prover/values-node1-prover.yaml prover/
 echo "✅ Node1 Prover instalado com sucesso!"
 
 echo "🔹 Instalando Node1 Node..."
-helm install node1-node --namespace brldigital-t -f node/values-node1-node.yaml node/
+helm upgrade --install node1-node --namespace brldigital-t -f node/values-node1-node.yaml node/
 echo "✅ Node1 Node instalado com sucesso!"
 
 echo "🚀 Todas as instalações foram concluídas!"
--- create_node3.sh
+++ create_node3.sh
@@ -1,21 +1,21 @@
 echo "🔹 Instalando Node3 MongoDB..."
-helm install node3-mongo --namespace brldigital-t -f mongodb/values-node3-mongodb.yaml mongodb/
+helm upgrade --install node3-mongo --namespace brldigital-t -f mongodb/values-node3-mongodb.yaml mongodb/
 echo "✅ Node3 MongoDB instalado com sucesso!"
 
 echo "🔹 Instalando Node3 PostgreSQL..."
-helm install node3-postgre --namespace brldigital-t -f postgres/values-node3-postgresql.yaml postgres/
+helm upgrade --install node3-postgre --namespace brldigital-t -f postgres/values-node3-postgresql.yaml postgres/
 echo "✅ Node3 PostgreSQL instalado com sucesso!"
 
 echo "🔹 Instalando Node3 Executor..."
-helm install node3-executor --namespace brldigital-t -f executor/values-node3-executor.yaml executor/
+helm upgrade --install node3-executor --namespace brldigital-t -f executor/values-node3-executor.yaml executor/
 echo "✅ Node3 Executor instalado com sucesso!"
 
 echo "🔹 Instalando Node3 Prover..."
-helm install node3-prover --namespace brldigital-t -f prover/values-node3-prover.yaml prover/
+helm upgrade --install node3-prover --namespace brldigital-t -f prover/values-node3-prover.yaml prover/
 echo "✅ Node3 Prover instalado com sucesso!"
 
 echo "🔹 Instalando Node3 Node..."
-helm install node3-node --namespace brldigital-t -f node/values-node3-node.yaml node/
+helm upgrade --install node3-node --namespace brldigital-t -f node/values-node3-node.yaml node/
 echo "✅ Node3 Node instalado com sucesso!"
 
 echo "🚀 Todas as instalações foram concluídas!"
